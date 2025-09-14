bae2f81986e3bd559218907b1545f63b998d61f0
Sara Vázquez <saravazlo@gmail.com>
2025-09-14 17:15:30 +0200

    style: added and fixed aside styles

--- src/components/buttonText/ButtonText.jsx
+++ src/components/buttonText/ButtonText.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import './ButtonText.css';
 
-export default function ButtonText({ text, onClick, isSelected }) {
+export default function ButtonText({ children, onClick, isSelected }) {
     const buttonClass = `btn-text ${isSelected ? 'selected' : ''}`;
     return (
-        <button className={buttonClass} onClick={onClick}>{text}</button>
+        <button className={buttonClass} onClick={onClick}>{children}</button>
     );
 }
