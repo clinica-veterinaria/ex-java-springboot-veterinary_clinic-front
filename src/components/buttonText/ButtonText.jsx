0f98e3e98de14acf987015822b218cc2881fbb6e
Sara Vázquez <saravazlo@gmail.com>
2025-09-14 13:44:01 +0200

    style: updated text buttons style

--- src/components/buttonText/ButtonText.jsx
+++ src/components/buttonText/ButtonText.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import './ButtonText.css';
 
-export default function ButtonText({ text, onClick }) {
+export default function ButtonText({ text, onClick, isSelected }) {
+    const buttonClass = `btn-text ${isSelected ? 'selected' : ''}`;
     return (
-        <button className="btn-text" onClick={onClick}>{text}</button>
+        <button className={buttonClass} onClick={onClick}>{text}</button>
     );
 }
