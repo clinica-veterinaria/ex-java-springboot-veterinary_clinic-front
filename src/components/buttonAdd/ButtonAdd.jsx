3df936e9820f138a5bbdfde20e448aef10719652
Sara Vázquez <saravazlo@gmail.com>
2025-09-14 13:01:37 +0200

    style: updated add button syles

--- src/components/buttonAdd/ButtonAdd.jsx
+++ src/components/buttonAdd/ButtonAdd.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import './ButtonAdd.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons"; 
 
 export default function ButtonAdd() {
     return(
         <button className="add">
-            
+            <FontAwesomeIcon icon={faPlus} className="faPlus"/>        
         </button>
 
     );
