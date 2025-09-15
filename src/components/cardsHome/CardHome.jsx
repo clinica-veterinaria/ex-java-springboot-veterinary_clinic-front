58f555ecdfc650474058a28f540140b56db0c090
Sara Vázquez <saravazlo@gmail.com>
2025-09-15 12:30:59 +0200

    feat: created the template for cards home

---
+++ src/components/cardsHome/CardHome.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import './CardHome.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCalendarCheck, faPaw } from "@fortawesome/free-solid-svg-icons";
+
+export default function CardHome({ variant = "appointments", onClick, ...props }) {
+
+    const config = {
+        appointments: {
+            icon: faCalendarCheck,
+            title: "Citas"
+        },
+
+        patients: {
+            icon: faPaw,
+            title: "Pacientes"
+        }
+    };
+
+    const { icon, title } = config[variant] || {}
+
+return(
+    <div className="card-home" onClick={onClick} {...props}>
+        <div className="card-home__image">
+        <FontAwesomeIcon icon={icon} className="card-home__icon" />
+        </div>
+        <div className="card-home__title">
+            <h2>{title}</h2>
+        </div>
+
+    </div>
+);
+}
