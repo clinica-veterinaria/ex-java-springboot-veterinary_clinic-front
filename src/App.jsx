63456bda9d2e7ae3b940abc44a3eb280aa781cc7
Sara Vázquez <saravazlo@gmail.com>
2025-09-16 22:09:36 +0200

    feat: updated and fixed appointmentCard

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,11 @@ import React from 'react'
 import './App.css'
 import './components/styles/Variables.css';
 import { BrowserRouter } from 'react-router-dom';
-import SignInPage from './pages/SignInPage';
-
 
 function App() {
   return (
     <BrowserRouter>
-      <SignInPage></SignInPage>
+    
     </BrowserRouter>
   )
 }
--- src/components/appointmentCard/AppointmentCard.jsx
+++ src/components/appointmentCard/AppointmentCard.jsx
@@ -6,12 +6,23 @@ import { faEllipsis } from "@fortawesome/free-solid-svg-icons";
 
 export default function AppointmentCard({ date, petName, reason, status, type, onMoreOptions }) {
 
-    // backend sends the expirate state
-    const cardClass = `appointment-card ${type === 'urgente' ? 'appointment-card--urgente' : ''} ${status === 'expirada' ? 'appointment-card--expired' : ''}`;
+    if (status === 'expirada') {
+        return (
+            <div className="appointment-card appointment-card--expired">
+                <div className="appointment-card__expired-container">
+                    <div className="appointment-card__date">{date}</div>
+                    <div className="appointment-card__pet">{petName}</div>
+                    <div className="appointment-card__reason">{reason}</div>
+                </div>
+            </div>
+        );
+    }
 
+    // backend sends the expired state
+    const cardClass = `appointment-card ${type === 'urgente' ? 'appointment-card--urgent' : ''}`;
 
     return(
-        <div className="appointment-card">
+        <div className={cardClass}>
             <div className="appointment-card__container">
                 <div className="appointment-card__date">{date}</div>
                 <div className="appointment-card__pet">{petName}</div>
