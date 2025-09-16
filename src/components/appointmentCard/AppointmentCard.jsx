41407e1bfbbafc7b090f0d7fac5383184097e563
Sara Vázquez <saravazlo@gmail.com>
2025-09-16 19:04:01 +0200

    feat: updated appointment class with functionalities

--- src/components/appointmentCard/AppointmentCard.jsx
+++ src/components/appointmentCard/AppointmentCard.jsx
@@ -1,10 +1,28 @@
 import React from "react";
 import './AppointmentCard.css';
+import ButtonStatus from '../buttonStatus/ButtonStatus';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEllipsis } from "@fortawesome/free-solid-svg-icons"; 
 
-export default function AppointmentCard() {
+export default function AppointmentCard({ date, petName, reason, status, type, onMoreOptions }) {
+
+    // backend sends the expirate state
+    const cardClass = `appointment-card ${type === 'urgente' ? 'appointment-card--urgente' : ''} ${status === 'expirada' ? 'appointment-card--expired' : ''}`;
+
+
     return(
         <div className="appointment-card">
-            
+            <div className="appointment-card__container">
+                <div className="appointment-card__date">{date}</div>
+                <div className="appointment-card__pet">{petName}</div>
+                <div className="appointment-card__reason">{reason}</div>
+                <div className="appointment-card__state">
+                    <ButtonStatus status={status} />
+                </div>
+                <div className="appointment-card__icon" onClick={onMoreOptions}>
+                   <FontAwesomeIcon icon={faEllipsis} className="faEllipsis"/>        
+                </div>
+            </div>
         </div>
     );
 }
