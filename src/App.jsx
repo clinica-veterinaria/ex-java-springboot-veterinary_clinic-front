0623fc9a3ce282166bb0950d31e307ee1b1c232d
Sara Vázquez <saravazlo@gmail.com>
2025-09-16 18:30:06 +0200

    feat: created file appointment card

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
 import React from 'react'
 import './App.css'
 import './components/styles/Variables.css';
-import SideMenuAdmin from './components/sideMenuAdmin/SideMenuAdmin';
 import { BrowserRouter } from 'react-router-dom';
-import SearchInput from './components/searchInput/SearchInput';
 import SignInPage from './pages/SignInPage';
 
 
---
+++ src/components/appointmentCard/AppointmentCard.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import './AppointmentCard.css';
+
+export default function AppointmentCard() {
+    return(
+        <div className="appointment-card">
+            
+        </div>
+    );
+}
