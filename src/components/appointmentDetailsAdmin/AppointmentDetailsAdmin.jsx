655722abaa32f41dd6bc449e69b88afd781aae5a
Sara Vázquez <saravazlo@gmail.com>
2025-09-22 14:40:37 +0200

    fix: updated appointment admin details with urgent method

--- src/components/appointmentDetailsAdmin/AppointmentDetailsAdmin.jsx
+++ src/components/appointmentDetailsAdmin/AppointmentDetailsAdmin.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import './AppointmentDetailsAdmin.css';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faXmark, faArrowUpRightFromSquare } from "@fortawesome/free-solid-svg-icons"; 
@@ -8,9 +8,12 @@ import Button from '../buttons/Button';
 import ButtonText from '../buttonText/ButtonText';
 
 export default function AppointmentDetailsAdmin({ onClose, petName, appointmentDate, appointmentTime, description, icon}) {
+    const [status, setStatus] = useState("pendiente");
+
+
     return(
         <div className="appointment-admin__overlay">
-        <div className="appointment-admin__container">
+        <div className={`appointment-admin__container ${status === 'urgente' ? 'appointment-admin__container--urgent' : ''}`}>
             <button className="appointment-admin__close" onClick={onClose} aria-label="Cerrar detalles de la cita">
                 <FontAwesomeIcon icon={faXmark} className="faXmark"/>        
             </button>
@@ -21,7 +24,7 @@ export default function AppointmentDetailsAdmin({ onClose, petName, appointmentD
                 <h3 className="appointment-admin__petName">{petName}</h3>
                 <div className="appointment-admin__data">
                 <PillDateTime date={appointmentDate} time={appointmentTime} />
-                <ButtonStatus />
+                <ButtonStatus initialStatus={status} onStatusChange={setStatus} />
                 </div>
                 <div className="appointment-admin__body">
                     <p className="appointment-admin__subtitle">Motivo</p>
@@ -29,8 +32,8 @@ export default function AppointmentDetailsAdmin({ onClose, petName, appointmentD
                     <ButtonText icon={<FontAwesomeIcon icon={faArrowUpRightFromSquare}/>}>Ver ficha paciente</ButtonText>
                 </div>
                 <div className="appointment-admin__buttons">
-                    <Button variant="secondary">Urgente</Button>
-                    <Button variant="primary">Estandar</Button>
+                    <Button variant="secondary" onClick={() => setStatus("urgente")}>Urgente</Button>
+                    <Button variant="primary" onClick={() => setStatus("estandar")}>Estándar</Button>
                 </div>
             </div>
         </div>
