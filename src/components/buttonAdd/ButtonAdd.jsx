bcf3b4bee8ccf7498f2ce607a4a0101272211206
Sara Vázquez <saravazlo@gmail.com>
2025-09-22 23:19:19 +0200

    feat: added generic props to add button

--- src/components/buttonAdd/ButtonAdd.jsx
+++ src/components/buttonAdd/ButtonAdd.jsx
@@ -3,9 +3,9 @@ import './ButtonAdd.css';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus } from "@fortawesome/free-solid-svg-icons"; 
 
-export default function ButtonAdd() {
+export default function ButtonAdd(props) {
     return(
-        <button className="add">
+        <button className="add" {...props}>
             <FontAwesomeIcon icon={faPlus} className="faPlus"/>        
         </button>
 
