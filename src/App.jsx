c75c95767290394afbe2dd5a9947c5887152d8fe
Sara Vázquez <saravazlo@gmail.com>
2025-09-22 22:59:12 +0200

    fix: fixed vulnerabilities with app jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,7 @@ import './App.css'
 import './components/styles/Variables.css';
 import { BrowserRouter } from 'react-router-dom';
 import AppointmentsPage from './pages/AppointmentsPage';
-import { CardPatient } from './components/cardPatient/CardPatient';
-import PatientPage from './pages/PatientPage';
+
 
 function App() {
   return (
