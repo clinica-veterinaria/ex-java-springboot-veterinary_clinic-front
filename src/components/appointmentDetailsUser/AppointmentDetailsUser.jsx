249bb58f2d5169397a472c7cb6790f607e8acf3e
Sara Vázquez <saravazlo@gmail.com>
2025-09-22 09:53:37 +0200

    style: added styles to user appointment details

--- src/components/appointmentDetailsUser/AppointmentDetailsUser.jsx
+++ src/components/appointmentDetailsUser/AppointmentDetailsUser.jsx
@@ -2,25 +2,26 @@ import React from 'react';
 import './AppointmentDetailsUser.css';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faXmark } from "@fortawesome/free-solid-svg-icons"; 
+import PillDateTime from '../pillDateTime/PillDateTime';
 
 
-export default function AppointmentDetailsUser({onClose, petName, description}) {
+export default function AppointmentDetailsUser({onClose, petName, description, appointmentDate, appointmentTime}) {
     return(
     <div className="appointment-user__container">
-        <div className="appointment-card__icon" onClick={onClose}>
+        <button className="appointment-card__icon" onClick={onClose} aria-label="Cerrar detalles de la cita">
             <FontAwesomeIcon icon={faXmark} className="faXmark"/>        
-        </div>
+        </button>
         <div className="appointment-user__title">
             <h2>Datos de la cita</h2>
         </div>
         <div className="appointment-user__details">
-            <h3>{`${petName}`}</h3>
+            <h3 className="appointment-user__petName">{petName}</h3>
             <div className="appointment-user__date">
-
+            <PillDateTime date={appointmentDate} time={appointmentTime} />
             </div>
             <div className="appointment-user__text">
                 <p className="appointment-user__subtitle">Motivo</p>
-                <p>{`${description}`}</p>
+                <p>{description}</p>
             </div>
         </div>
     </div>
