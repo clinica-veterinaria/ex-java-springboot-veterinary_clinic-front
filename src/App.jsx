db41e0e26a58194c92051432fcfa8d90df3e572a
Sara Vázquez <saravazlo@gmail.com>
2025-09-22 11:28:39 +0200

    style: updated styles for user appointment details

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,17 @@ import React from 'react'
 import './App.css'
 import './components/styles/Variables.css';
 import { BrowserRouter } from 'react-router-dom';
+import AppointmentDetailsUser from './components/appointmentDetailsUser/AppointmentDetailsUser';
 
 function App() {
   return (
     <BrowserRouter>
-    
+      <AppointmentDetailsUser 
+      petName="Pepita"
+      description="Revisión anual y vacuna"
+      appointmentDate="2025-09-11"
+      appointmentTime="12:00"
+      />
     </BrowserRouter>
   )
 }
--- src/components/appointmentDetailsUser/AppointmentDetailsUser.jsx
+++ src/components/appointmentDetailsUser/AppointmentDetailsUser.jsx
@@ -7,8 +7,9 @@ import PillDateTime from '../pillDateTime/PillDateTime';
 
 export default function AppointmentDetailsUser({onClose, petName, description, appointmentDate, appointmentTime}) {
     return(
+    <div className="appointment-user__overlay">
     <div className="appointment-user__container">
-        <button className="appointment-card__icon" onClick={onClose} aria-label="Cerrar detalles de la cita">
+        <button className="appointment-user__close" onClick={onClose} aria-label="Cerrar detalles de la cita">
             <FontAwesomeIcon icon={faXmark} className="faXmark"/>        
         </button>
         <div className="appointment-user__title">
@@ -19,11 +20,12 @@ export default function AppointmentDetailsUser({onClose, petName, description, a
             <div className="appointment-user__date">
             <PillDateTime date={appointmentDate} time={appointmentTime} />
             </div>
-            <div className="appointment-user__text">
+            <div className="appointment-user__body">
                 <p className="appointment-user__subtitle">Motivo</p>
-                <p>{description}</p>
+                <p className="appointment-user__text">{description}</p>
             </div>
         </div>
+    </div>
     </div>
     );
 }
