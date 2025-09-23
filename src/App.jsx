7b43a5009df738e144c865f1854d409688720239
Paula Fernandez Aranda <paulafa8@gmail.com>
2025-09-23 12:37:15 +0200

    fix: padding appointment page

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css'
 import './components/styles/Variables.css';
+import SideMenuAdmin from './components/sideMenuAdmin/SideMenuAdmin';
 import { BrowserRouter } from 'react-router-dom';
+import SearchInput from './components/searchInput/SearchInput';
 import { testConnection, testPatientCreation } from './services/APIPatient';
-import PatientPage from './pages/PatientPage';
+import AppointmentsPage from './pages/AppointmentsPage';
 
 
 function App() {
 
   return (
     <BrowserRouter>
-    <PatientPage />
+    <AppointmentsPage />
     </BrowserRouter>
   )
 }
