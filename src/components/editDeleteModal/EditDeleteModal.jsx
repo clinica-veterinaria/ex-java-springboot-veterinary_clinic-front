6d9a91aeb7108f99c1236b327ff3d5e01862286b
Sara Vázquez <saravazlo@gmail.com>
2025-09-23 09:36:11 +0200

    style: added styles to edit-delete modal

--- src/components/editDeleteModal/EditDeleteModal.jsx
+++ src/components/editDeleteModal/EditDeleteModal.jsx
@@ -6,8 +6,8 @@ export default function EditDeleteModal({ onGoToEdit, onGoToDelete, onClose }) {
         <div className="editDelete__overlay" onClick={onClose}>
             <div className="editDelete__container" role="dialog" aria-modal="true" onClick={(e) => e.stopPropagation()}>
                 <div className="editDelete__actions">
-                    <button type="button" className="modal-options__link" onClick={onGoToEdit}>Editar</button>
-                    <button type="button" className="modal-options__link" onClick={onGoToDelete}>Eliminar</button>
+                    <button type="button" className="editDelete__link" onClick={onGoToEdit}>Editar</button>
+                    <button type="button" className="editDelete__link" onClick={onGoToDelete}>Eliminar</button>
                 </div>
             </div>
         </div>
