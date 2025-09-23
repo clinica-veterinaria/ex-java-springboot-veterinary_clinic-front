484a8869f3e19a86a31f3d920ab00d72da6ad338
Sara Vázquez <saravazlo@gmail.com>
2025-09-23 14:18:09 +0200

    fix: updated home page structure

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import './HomePage.css';
 import SideMenuAdmin from "../components/sideMenuAdmin/SideMenuAdmin";
 import Navbar from "../components/navbar/Navbar";
+import CardHome from "../components/cardsHome/CardHome";
 
 export default function HomePage(){
     return(
@@ -15,7 +16,18 @@ export default function HomePage(){
                 </div>
                 <div className="home-page__container">
                     <div className="home-page__content">
-                        
+                        <div className="home-page__planning">
+                            <div className="home-page__week-view">
+
+                            </div>
+                            <div className="home-page__next-appointments">
+                                
+                            </div>
+                        </div>
+                        <div className="home-page__cards">
+                            <CardHome variant="appointments" />
+                            <CardHome variant="pets"/>
+                        </div>
                     </div>
                 </div>
 
