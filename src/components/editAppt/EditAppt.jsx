3eb3aa0f809dcceb3bf0424abeb61b79bc1ac534
Sara Vázquez <saravazlo@gmail.com>
2025-09-23 08:24:54 +0200

    feat: created edit appointment modal structure

---
+++ src/components/editAppt/EditAppt.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './EditAppt.css';
+
+export default function EditAppt() {
+    return(
+        <div className="edit-appointment__overlay">
+            <div className="edit-appointment__container">
+                
+            </div>
+        </div>
+    );
+}
